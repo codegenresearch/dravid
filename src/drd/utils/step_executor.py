cee6fa30e022ff813ed960c513f144090c49773b
venkatkrishna <venkatkrishna@moe>
2025-02-09 19:01:25 +0100

    Reset to a2913f0c9ac2e7260c0e7f6dfbea6a9380be996a and made changes for LLM Round 1

--- src/drd/utils/step_executor.py
+++ src/drd/utils/step_executor.py
@@ -14,6 +14,7 @@ from ..metadata.common_utils import get_ignore_patterns, get_folder_structure
 class Executor:
     def __init__(self):
         self.current_dir = os.getcwd()
+        self.initial_dir = self.current_dir
         self.allowed_directories = [self.current_dir, '/fake/path']
         self.disallowed_commands = [
             'rmdir', 'del', 'format', 'mkfs',
@@ -21,10 +22,9 @@ class Executor:
             'sudo', 'su', 'chown', 'chmod'
         ]
         self.env = os.environ.copy()
-        self.initial_dir = self.current_dir
 
     def is_safe_path(self, path):
-        full_path = os.path.abspath(os.path.join(self.current_dir, path))
+        full_path = os.path.abspath(path)
         return any(full_path.startswith(allowed_dir) for allowed_dir in self.allowed_directories) or full_path == self.current_dir
 
     def is_safe_rm_command(self, command):
@@ -286,7 +286,7 @@ class Executor:
 
     def _handle_cd_command(self, command):
         _, path = command.split(None, 1)
-        new_dir = os.path.abspath(os.path.join(self.current_dir, path))
+        new_dir = os.path.abspath(path)
         if self.is_safe_path(new_dir):
             os.chdir(new_dir)
             self.current_dir = new_dir
